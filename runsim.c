292541309c27960ca19ac505c5300580cd674baf
Connor Schultz <deltaboltz@me.com>
2020-09-21 20:28:24 -0500

    Update runsim.c
    moved main before written functions

--- runsim.c
+++ runsim.c
@@ -20,62 +20,6 @@ void print_usage();
 
 int* parse_cmd_line_args(int argc, char *argv[]);
 
-char** get_exec_argv(char* str)
-{
-	int i = 0;
-	char* substr;
-	char** exec_argv = malloc(10 * sizeof(char));
-
-	substr = strtok(str, " ");
-
-	while(substr != NULL)
-	{
-		exec_argv[i] = malloc(20 * sizeof(char));
-		exec_argv[i] = substr;
-		substr = strtok(NULL, " ");
-		i++;
-	}
-	exec_argv[i] == NULL;
-
-	return exec_argv; 
-}
-
-int* parse_cmd_line_args(int argc, char *argv[])
-{	
-	int option;
-
-	int* procLimit = malloc(sizeif(int));
-
-	if(argc < 2)
-	{
-		print_usage();
-	}
-	
-	while((option = getopt(argc, argv, "n:h")) != -1)
-	switch(option)
-	{
-		case 'h':
-			print_usage();
-		
-		case 'n':
-			*procLimit = atoi(optarg);
-			break;
-		
-		default:
-			print_usage();
-	}
-
-	return procLimit;
-
-
-}
-
-void print_usage()
-{
-	fprintf(stderr, "-n is the max number of concurrent children allowed");
-	exit(2);
-}
-
 int main(int argc, char *argv[])
 {
 	//Limit set by cmdln arguments
@@ -149,3 +93,61 @@ int main(int argc, char *argv[])
 	free(procLimit);
 	return 0;
 }
+
+char** get_exec_argv(char* str)
+{
+	int i = 0;
+	char* substr;
+	char** exec_argv = malloc(10 * sizeof(char));
+
+	substr = strtok(str, " ");
+
+	while(substr != NULL)
+	{
+		exec_argv[i] = malloc(20 * sizeof(char));
+		exec_argv[i] = substr;
+		substr = strtok(NULL, " ");
+		i++;
+	}
+	exec_argv[i] == NULL;
+
+	return exec_argv; 
+}
+
+int* parse_cmd_line_args(int argc, char *argv[])
+{	
+	int option;
+
+	int* procLimit = malloc(sizeif(int));
+
+	if(argc < 2)
+	{
+		print_usage();
+	}
+	
+	while((option = getopt(argc, argv, "n:h")) != -1)
+	switch(option)
+	{
+		case 'h':
+			print_usage();
+		
+		case 'n':
+			*procLimit = atoi(optarg);
+			break;
+		
+		default:
+			print_usage();
+	}
+
+	return procLimit;
+
+
+}
+
+void print_usage()
+{
+	fprintf(stderr, "-n is the max number of concurrent children allowed");
+	exit(2);
+}
+
+
