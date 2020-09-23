81371fc8aaa3246beb0feadf1a74b8fcb9064826
Connor Schultz <deltaboltz@me.com>
2020-09-22 21:24:01 -0500

    Update runsim.c
    code change from old commit

--- runsim.c
+++ runsim.c
@@ -8,126 +8,111 @@
 #include <getopt.h>
 
 const int MAX_CANON = 10;
+
 char** get_exec_argv(char* str);
 void print_usage();
-
 int* parse_cmd_line_args(int argc, char *argv[]);
 
-int main(int argc, char *argv[]){
-	//Limit set by cmdln arguments
-	int* proc_limit;
-	
+int main (int argc, char *argv[]) {
+    int* proc_limit;        
+    int proc_count = 0;     
+    
+    char cmd[MAX_CANON];
+    pid_t childpid = 0;     
+    char** exec_argv;       
 
-	//Number of children allowed
-	int proc_count = 0;
-	char cmd[MAX_CANON];
-	
-	//Child proc id
-	pid_t childpid = 0;
-	char** exec_argv;
-	
-	proc_limit = parse_cmd_line_args(argc, argv);
-	
-	
-	while(fgets(cmd, MAX_CANON, stdin)){
-		if(proc_count == *proc_limit){
-			wait(NULL);
-			proc_count -= 1;
-		}
-	
-	
-		//Check to see if we fork a child
-		if((childpid = fork()) == 0){
-			//Remove newlines
-			strtok(cmd, "\n");
-		
-			//pass execvp
-			exec_argv = get_exec_argv(cmd);
+    proc_limit = parse_cmd_line_args(argc, argv);
 
-			//execvp
-			execvp(exec_argv[0], exec_argv);
+    while (fgets(cmd, MAX_CANON, stdin)) {
+        
+        if (proc_count == *proc_limit) {
+            
+            wait(NULL);
+            proc_count -= 1;
+        }
 
-			//print perror
-			perror("Child fault: execvp failed");
-			return 1;
-		}
+        if ((childpid = fork()) == 0) {
+           
+            strtok(cmd, "\n"); // remove \n's
 
-		//Check to see if fork failed
-		if(childpid == -1){
-			perror("Child fault: fork failed");
-			return 1;
-		}
-		
-		//Increment procCount since we forked
-		proc_count += 1;
-	
+            exec_argv = get_exec_argv(cmd);
 
-		//Wait check, did child finish?
-		if(waitpid(-1, NULL, WNOHANG) > 0){
-			//Child finished, decrement
-			proc_count -= 1;
-		}
-	}
+            execvp(exec_argv[0], exec_argv);
+            
+            perror("Child failed to execvp the command");
+            return 1;
+        }
+        
+        if (childpid == -1) {
+            perror("Child failed to fork!\n");
+            return 1;
+        }
+        
+        proc_count += 1; 
 
-	//check to see if childpid is > 0
-	if(childpid > 0){
-		//if so while wait(NULL) > 0
-		while(wait(NULL) > 0);
-	}
+        if (waitpid(-1, NULL, WNOHANG) > 0) {
+            // A child has finished executing
+            proc_count -= 1;
+        }
 
-	//deallocate procLimit
-	free(proc_limit);
-	return 0;
+    }
+    
+    if (childpid > 0) {
+        while (wait(NULL) > 0); 
+    }
+
+    free(proc_limit);
+
+    return 0;
+
 }
 
-char** get_exec_argv(char* str){
-	
-	char* substr;
-	char** exec_argv = malloc(10 * sizeof(char));
+char** get_exec_argv(char* str) {
 
-	substr = strtok(str, " ");
+    char* substr;
+    char** exec_argv = malloc(10 * sizeof(char));
 
-	int i = 0;
-	while(substr != NULL){
-		exec_argv[i] = malloc(20 * sizeof(char));
-		exec_argv[i] = substr;
-		substr = strtok(NULL, " ");
-		i++;
-	}
-	exec_argv[i] = NULL;
+    substr = strtok(str, " ");
 
-	return exec_argv; 
+    int i = 0;
+    while (substr != NULL)
+    {
+        exec_argv[i] = malloc(20 * sizeof(char));
+        exec_argv[i] = substr;
+        substr = strtok(NULL, " ");
+        i++;
+    }
+    exec_argv[i] = NULL;
+
+    return exec_argv;
+
 }
 
-int* parse_cmd_line_args(int argc, char *argv[]){	
-	
+int* parse_cmd_line_args(int argc, char *argv[]) {
+    int* proc_limit = malloc(sizeof(int));
 
-	int* proc_limit = malloc(sizeof(int));
+    if (argc < 2) {
+        print_usage();
+    }
 
-	if(argc < 2){
-		print_usage();
-	}
-	
-	int option;
-	while((option = getopt(argc, argv, "n:h")) != -1)
-	switch(option){
-		case 'h':
-			print_usage();
-		
-		case 'n':
-			*proc_limit = atoi(optarg);
-			break;
-		
-		default:
-			print_usage();
-	}
+    int option;
+    while ((option = getopt (argc, argv, "n:h")) != -1)
+    switch (option) {
+        case 'h':
+            print_usage();
+        case 'n':
+            *proc_limit = atoi(optarg);
+            break;
+        default:
+            print_usage();
+    }
 
-	return proc_limit;
+    return proc_limit;
+
 }
 
-void print_usage(){
-	fprintf(stderr, "-n is the max number of concurrent children allowed");
-	exit(2);
+void print_usage() {
+    fprintf(stderr, "Usage: runsim -n max number of concurrent children\n");
+    exit(2);
 }
-
 
